7cec3c3affa0cc55e362566933ea1fcb0efe7a8c
cntam <tam@efortunetech.com>
2018-07-03 11:29:02 +0800

    20180703 potem8-9

--- highable/potem9.php
+++ highable/potem9.php
@@ -18,23 +18,23 @@ use PhpOffice\PhpSpreadsheet\Helper\Html as HtmlHelper; // html 解析器
 use PhpOffice\PhpSpreadsheet\Helper\Sample;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 
-$potem8 =  $_SESSION['potem8'];
+$potem9 =  $_SESSION['potem9'];
 
 
 //$spreadsheet = new Spreadsheet();
-$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('../template/potem8.xlsx');
+$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load('../template/potem9.xlsx');
 
 $sheet = $spreadsheet->getActiveSheet();
 $spreadsheet->getActiveSheet()->setTitle("sheet1");
 $spreadsheet->getDefaultStyle()->getFont()->setName('Microsoft YaHei');
 //$spreadsheet->getActiveSheet()->getDefaultColumnDimension()->setWidth(20);  //设置默认列宽
 $spreadsheet->getActiveSheet()->getColumnDimension('A')->setWidth(25);  //列宽度
-$spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(55);  //列宽度
-$spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(30);  //列宽度
-$spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(15);  //列宽度
-$spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(35);  //列宽度
-//$spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(25);  //列宽度
-//$spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(16);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('B')->setWidth(30);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('C')->setWidth(25);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('D')->setWidth(25);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('E')->setWidth(20);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('F')->setWidth(15);  //列宽度
+$spreadsheet->getActiveSheet()->getColumnDimension('G')->setWidth(30);  //列宽度
 //$spreadsheet->getActiveSheet()->getColumnDimension('H')->setWidth(15);  //列宽度
 //$spreadsheet->getActiveSheet()->getColumnDimension('I')->setWidth(15);  //列宽度
 $spreadsheet->getDefaultStyle()->getFont()->setSize(7);
@@ -104,73 +104,73 @@ $styleArraybu = [
 ];
 
 //填数据
-$spreadsheet->getActiveSheet()->setCellValue('B5', $potem8["tosb"]);
-$spreadsheet->getActiveSheet()->getStyle('B5')->applyFromArray($styleArray2);
-$spreadsheet->getActiveSheet()->setCellValue('E6', $potem8["podate"]);
-$spreadsheet->getActiveSheet()->setCellValue('B6', $potem8["toaddr"]["a1"]);
-$spreadsheet->getActiveSheet()->setCellValue('B10', $potem8["toaddr"]["a2"]);
-$spreadsheet->getActiveSheet()->setCellValue('B7', $potem8["toaddr"]["a3"]);
-$spreadsheet->getActiveSheet()->setCellValue('B8', $potem8["toaddr"]["a4"]);
-$spreadsheet->getActiveSheet()->setCellValue('B9', $potem8["toaddr"]["a5"]);
-$spreadsheet->getActiveSheet()->setCellValue('E9', $potem8["toaddr"]["a6"]);
+$spreadsheet->getActiveSheet()->setCellValue('B7', $potem9["tosb"]);
+$spreadsheet->getActiveSheet()->setCellValue('F7', $potem9["podate"]);
+$spreadsheet->getActiveSheet()->setCellValue('B8', $potem9["toaddr"]["a1"]);
+$spreadsheet->getActiveSheet()->setCellValue('F8', $potem9["toaddr"]["a2"]);
+$spreadsheet->getActiveSheet()->setCellValue('B9', $potem9["toaddr"]["a3"]);
+$spreadsheet->getActiveSheet()->setCellValue('F9', $potem9["toaddr"]["a4"]);
+$spreadsheet->getActiveSheet()->setCellValue('B10', $potem9["toaddr"]["a5"]);
+$spreadsheet->getActiveSheet()->setCellValue('F10', $potem9["toaddr"]["a6"]);
 
 
 //中部form
 
-$nowcol = 11;
+$nowcol = 12;
 //$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:F{$nowcol}");
-$spreadsheet->getActiveSheet()->setCellValue('B'.$nowcol, $potem8["orderform"]["midpono"]);
-//$spreadsheet->getActiveSheet()->setCellValue('I'.$nowcol, $potem8["invoiceform"]["amout"]);
+$spreadsheet->getActiveSheet()->setCellValue('A'.$nowcol, 'PO NO: '.$potem9["orderform"]["midpono"].'   注：請在開發票時把“PONO”寫上，不可重復，并且寫上制單號）');
+//$spreadsheet->getActiveSheet()->setCellValue('I'.$nowcol, $potem9["invoiceform"]["amout"]);
 
 
-for($x = 0 ,$c = 1; $c <= $potem8["orderform"]["formnum"]; $x++ ,$c++){
+for($x = 0 ,$c = 1; $c <= $potem9["orderform"]["formnum"]; $x++ ,$c++){
 
-$f19 = 19 + 1 * $x;
+$f19 = 14 + 1 * $x;
 
 //$spreadsheet->getActiveSheet()->mergeCells("B{$f19}:E{$f19}");
 
-$formarr = array('A'.$f19,'B'.$f19,'C'.$f19,'D'.$f19,'E'.$f19);
+$formarr = array('A'.$f19,'B'.$f19,'C'.$f19,'D'.$f19,'E'.$f19,'F'.$f19,'G'.$f19);
 
-    for($i = 1,$y = 0; $i <= $potem8["orderform"]["brrnum"] ; $i++ ,$y++){
+    for($i = 1,$y = 0; $i <= $potem9["orderform"]["brrnum"] ; $i++ ,$y++){
 
-        $sheet->setCellValue($formarr[$y],  $potem8["orderform"]['b'.$i][$x]);
+        $sheet->setCellValue($formarr[$y],  $potem9["orderform"]['b'.$i][$x]);
 
     }
 
 
-    $nowcol = 19  +   1 * $c;
+    $nowcol = 14  +   1 * $c;
 
 
-    if($x >3){
+    if($x >2){
         $spreadsheet->getActiveSheet()->insertNewRowBefore($nowcol, 1);
     }
 
 }
-//$nowcol = $potem8["orderform"]["formnum"] > 14 ? ($nowcol + 1) : 26;
-////$spreadsheet->getActiveSheet()->getCell('A1')->setValue($nowcol); 貨送以下地址
-////$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
-////$spreadsheet->getActiveSheet()->setCellValue('A'.$nowcol, '貨送以下地址');
-////$nowcol++;
-//
+$nowcol = $potem9["orderform"]["formnum"] > 2 ? ($nowcol + 1) : 18;
+//$spreadsheet->getActiveSheet()->getCell('A1')->setValue($nowcol); 貨送以下地址
 //$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
-$spreadsheet->getActiveSheet()->setCellValue('A12', $potem8["remark"]["c1"]);
-
+//$spreadsheet->getActiveSheet()->setCellValue('A'.$nowcol, '貨送以下地址');
+//$nowcol++;
 
 //$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
-$spreadsheet->getActiveSheet()->setCellValue('A13', $potem8["remark"]["c2"]);
+$spreadsheet->getActiveSheet()->setCellValue('B'.$nowcol, $potem9["remark"]["c1"]);
+$nowcol++;
 
-////$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
-//$spreadsheet->getActiveSheet()->setCellValue('A'.$nowcol, $potem8["remark"]["c3"]);
-//
+//$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
+$spreadsheet->getActiveSheet()->setCellValue('B'.$nowcol, $potem9["remark"]["c2"]);
+$nowcol++;
+//$spreadsheet->getActiveSheet()->mergeCells("A{$nowcol}:E{$nowcol}");
+$spreadsheet->getActiveSheet()->setCellValue('E'.$nowcol, $potem9["remark"]["c3"]);
+$nowcol++;
 
+$spreadsheet->getActiveSheet()->setCellValue('A'.$nowcol, $potem9["remark"]["c4"]);
 
 $spreadsheet->getActiveSheet()->getPageSetup()->setFitToPage(true); //将工作表调整为一页
 
-unset($_SESSION['potem8'] ); //注销SESSION
+unset($_SESSION['potem9'] ); //注销SESSION
 
 $output=  ($_GET['action'] == 'formdown' )? 1:0;
 $nt = date("YmdHis",time()); //转换为日期。
-$filenameout = 'potem8out'.$nt.'.xlsx';
+$filenameout = 'potem9out'.$nt.'.xlsx';
 if($output){
     // Redirect output to a client’s web browser (Xlsx)
     header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
--- highable/session.php
+++ highable/session.php
@@ -2,7 +2,7 @@
 session_start();
 
 
-$samplep1 =   $_SESSION['potem8'];
+$samplep1 =   $_SESSION['potem9'];
 var_dump($samplep1);
 
 
