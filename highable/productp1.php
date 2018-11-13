f0ef5cf71aef6a087f66526babc27ccb3173263a
cntam <34434420+cntam@users.noreply.github.com>
2018-11-13 11:25:11 +0800

    201801113 productp1

--- highable/productp1.php
+++ highable/productp1.php
@@ -20,6 +20,39 @@ $sheet = $spreadsheet->getActiveSheet();
 $spreadsheet->getDefaultStyle()->getFont()->setName('Microsoft Yahei');
 $spreadsheet->getDefaultStyle()->getFont()->setSize(12);
 
+$styleArray = [
+
+    'alignment' => [
+        'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_LEFT,
+        'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+    ],
+
+    'borders' => [
+        'top' => [
+            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+        ],
+        'bottom' => [
+            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+        ],
+        'left' => [
+            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+        ],
+        'right' => [
+            'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN,
+        ],
+    ],
+
+];
+
+function isselect($value){
+    if ( $value == 'on') {
+        $output = '■  ';
+    } else {
+        $output = '□  ';
+    }
+    return $output;
+}
+
 
 $sheet->setCellValue('C2',  $productp1['guest']);
 $sheet->setCellValue('C3',  $productp1['alist']['a1']);
@@ -28,6 +61,129 @@ $sheet->setCellValue('I3',  $productp1['styleno']);
 $sheet->setCellValue('N2',  $productp1['sampleno']);
 $sheet->setCellValue('O3',  $productp1['alist']['a2']);
 
+
+/**
+ * 细数分配表
+ */
+for($v=0,$ct = 15;$v<8;$v++,$ct++){
+    $col = chr(68 + $v); //D
+
+    $sheet->setCellValue($col.'5',  $productp1['ctlist']['ct'.$ct]);
+    $spreadsheet->getActiveSheet()->getStyle($col.'5')->applyFromArray($styleArray);
+    $spreadsheet->getActiveSheet()->getStyle($col.'5')->getAlignment()->setWrapText(true);
+}
+
+$row = 7;
+for($i=0;$i<=$productp1['allot']['formnum'];$i++){
+
+    if($i==$productp1['allot']['formnum']){
+        $row = 12;
+        for($h=3;$h<=11;$h++){
+            $col = chr(65 + $h); //B
+            $sheet->setCellValue($col.$row,  $productp1['allot']['b'.$h][$i]);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->getAlignment()->setWrapText(true);
+        }
+    }else{
+        for($h=1;$h<=11;$h++){
+            if($h == 1){
+                $col = chr(64 + $h); //A
+            }else{
+                $col = chr(65 + $h); //B
+            }
+
+            $sheet->setCellValue($col.$row,  $productp1['allot']['b'.$h][$i]);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->getAlignment()->setWrapText(true);
+        }
+        $row++;
+    }
+}
+
+/**
+ *  //细数分配表
+ */
+
+/**
+ *  船头办数量
+ */
+for($i=0,$ct=1;$i< 14;$i++,$ct++){
+    if($ct == 14){
+            $sheet->setCellValue('A16',  '出船头办日期：'.$productp1['ctlist']['ct'.$ct]);
+            $spreadsheet->getActiveSheet()->getStyle('A16')->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle('A16')->getAlignment()->setWrapText(true);
+    }elseif($ct == 12 or $ct == 13){
+
+        if($ct == 12){
+            $sheet->setCellValue('C15',  $productp1['ctlist']['ct'.$ct]);
+            $spreadsheet->getActiveSheet()->getStyle('C15')->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle('C15')->getAlignment()->setWrapText(true);
+        }else{
+            $sheet->setCellValue('F15',  $productp1['ctlist']['ct'.$ct]);
+            $spreadsheet->getActiveSheet()->getStyle('F15')->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle('F15')->getAlignment()->setWrapText(true);
+        }
+
+    }else{
+        $row = 14;
+
+            if($ct == 1){
+                $col = chr(64 + $ct); //A
+            }else{
+                $col = chr(65 + $ct); //B
+            }
+
+            $sheet->setCellValue($col.$row,  $productp1['ctlist']['ct'.$ct]);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->applyFromArray($styleArray);
+            $spreadsheet->getActiveSheet()->getStyle($col.$row)->getAlignment()->setWrapText(true);
+
+    }
+}
+/**
+ *  //船头办数量
+ */
+
+/**
+ *  工艺
+ */
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab5']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('A22', $richText);
+$spreadsheet->getActiveSheet()->getStyle('A22')->getAlignment()->setWrapText(true);
+
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab6']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('A30', $richText);
+$spreadsheet->getActiveSheet()->getStyle('A30')->getAlignment()->setWrapText(true);
+
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab7']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('A54', $richText);
+$spreadsheet->getActiveSheet()->getStyle('A54')->getAlignment()->setWrapText(true);
+
+
+$sheet->setCellValue('B60',  '制单人:  '.$productp1['alist']['a4']);
+$spreadsheet->getActiveSheet()->getStyle($col.$row)->applyFromArray($styleArray);
+$spreadsheet->getActiveSheet()->getStyle($col.$row)->getAlignment()->setWrapText(true);
+/**
+ *  //工艺
+ */
+
+
+/**
+ *  特殊工艺
+ */
+$teshu = join(",",$productp1['alist']['a5value']);
+$sheet->setCellValue('M6',  $teshu);
+$spreadsheet->getActiveSheet()->getStyle('M6:P6')->applyFromArray($styleArray);
+$spreadsheet->getActiveSheet()->getStyle('M6:P6')->getAlignment()->setWrapText(true);
+
+/**
+ *  //特殊工艺
+ */
 ///**
 // *  一.主唛/烟治唛/产地唛车法：
 // */
@@ -39,71 +195,106 @@ $sheet->setCellValue('O3',  $productp1['alist']['a2']);
 //$spreadsheet->getActiveSheet() ->setCellValue('C18', $richText);
 //$spreadsheet->getActiveSheet()->getStyle('C18')->getAlignment()->setWrapText(true);
 //
-///*加載圖片*/
-//$img = $productp1['blist']['b3'];
-//if ($img == '') {
-//    $haveimg = false;  //没有图片
-//
-//} else {
-//
-//    $path = $img;
-//    $pathinfo = pathinfo($path);
-//    //echo "扩展名：$pathinfo[extension]";
-//
-//    if ($pathinfo["extension"] == 'pdf') {
-//
-//        $img = pdficon();
-//        $haveimg = true;
-//    } else {
-//        $haveimg = true;
-//    }
-//}
-//
-//
-//if ($haveimg) {
-//    preg_match('/.(jpg|gif|bmp|jpeg|png)/i', $img, $imgformat);
-//    $imgformat = $imgformat[1];
-//    switch ($imgformat) {
-//        case "jpg":
-//        case "jpeg":
-//            $img = imagecreatefromjpeg($img);
-//            break;
-//        case "bmp":
-//            $img = imagecreatefromwbmp($img);
-//            break;
-//        case "gif":
-//            $img = imagecreatefromgif($img);
-//            break;
-//        case "png":
-//            $img = imagecreatefrompng($img);
-//            break;
-//    }
-//    $width = imagesx($img);
-//    $height = imagesy($img);
-//
-//
-//// Add a drawing to the worksheet
-//    $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing();
-//    $drawing->setName('img');
-//    $drawing->setDescription('img');
-////$drawing->setImageResource($gdImage);
-//    $drawing->setImageResource($img);
-//    $drawing->setRenderingFunction(\PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing::RENDERING_JPEG);
-//    $drawing->setMimeType(\PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing::MIMETYPE_DEFAULT);
-////$drawing->setHeight($width);
-//
-//    //$drawing->setWidth($width>250 ? 250:$width);
-//    $drawing->setHeight($height > 170 ? 170 : $height);
-////$drawing->setHeight(150);
-//
-//    //$drawing->setCoordinates($cola.'2');
-//    $drawing->setCoordinates('C8');
-//    $drawing->setOffsetX(5);
-//    $drawing->setOffsetY(5);
-//    $drawing->setWorksheet($spreadsheet->getActiveSheet());
-//}
-//
-///*加載圖片*/
+/*加載圖片*/
+$img = $productp1['alist']['a3'];
+if ($img == '') {
+    $haveimg = false;  //没有图片
+
+} else {
+
+    $path = $img;
+    $pathinfo = pathinfo($path);
+    //echo "扩展名：$pathinfo[extension]";
+
+    if ($pathinfo["extension"] == 'pdf') {
+
+        $img = pdficon();
+        $haveimg = true;
+    } else {
+        $haveimg = true;
+    }
+}
+
+
+if ($haveimg) {
+    preg_match('/.(jpg|gif|bmp|jpeg|png)/i', $img, $imgformat);
+    $imgformat = $imgformat[1];
+    switch ($imgformat) {
+        case "jpg":
+        case "jpeg":
+            $img = imagecreatefromjpeg($img);
+            break;
+        case "bmp":
+            $img = imagecreatefromwbmp($img);
+            break;
+        case "gif":
+            $img = imagecreatefromgif($img);
+            break;
+        case "png":
+            $img = imagecreatefrompng($img);
+            break;
+    }
+    $width = imagesx($img);
+    $height = imagesy($img);
+
+
+// Add a drawing to the worksheet
+    $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing();
+    $drawing->setName('img');
+    $drawing->setDescription('img');
+//$drawing->setImageResource($gdImage);
+    $drawing->setImageResource($img);
+    $drawing->setRenderingFunction(\PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing::RENDERING_JPEG);
+    $drawing->setMimeType(\PhpOffice\PhpSpreadsheet\Worksheet\MemoryDrawing::MIMETYPE_DEFAULT);
+//$drawing->setHeight($width);
+
+    //$drawing->setWidth($width>250 ? 250:$width);
+    $drawing->setHeight($height > 170 ? 170 : $height);
+//$drawing->setHeight(150);
+
+    //$drawing->setCoordinates($cola.'2');
+    $drawing->setCoordinates('M7');
+    $drawing->setOffsetX(5);
+    $drawing->setOffsetY(5);
+    $drawing->setWorksheet($spreadsheet->getActiveSheet());
+}
+
+/*加載圖片*/
+
+
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab2']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('Q4', $richText);
+$spreadsheet->getActiveSheet()->getStyle('Q4:R14')->getAlignment()->setWrapText(true);
+
+/**
+ * 裁法
+ */
+$M16 = '单方向：'. isselect($productp1['alist']['a6']).'倒插：'.isselect($productp1['alist']['a7']).'女装：'.isselect($productp1['alist']['a8']);
+$sheet->setCellValue('M16',  $M16);
+$spreadsheet->getActiveSheet()->getStyle('M16:R16')->applyFromArray($styleArray);
+$spreadsheet->getActiveSheet()->getStyle('M16:R16')->getAlignment()->setWrapText(true);
+
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab3']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('M17', $richText);
+$spreadsheet->getActiveSheet()->getStyle('M17')->getAlignment()->setWrapText(true);
+
+$M19 = '过粘朴机 ：'. isselect($productp1['alist']['a9']);
+$sheet->setCellValue('M19',  $M19);
+$spreadsheet->getActiveSheet()->getStyle('M19:R19')->applyFromArray($styleArray);
+$spreadsheet->getActiveSheet()->getStyle('M19:R19')->getAlignment()->setWrapText(true);
+
+$wizard = new HtmlHelper();
+$html1 = str_replace('\"', "", $productp1['fablist']['fab4']) ;
+$richText = $wizard->toRichTextObject($html1);
+$spreadsheet->getActiveSheet() ->setCellValue('M20', '针距：'.$richText);
+$spreadsheet->getActiveSheet()->getStyle('M20:R20')->getAlignment()->setWrapText(true);
+/**
+ * //裁法
+ */
 //
 ///**
 // *  //一.主唛/烟治唛/产地唛车法：
