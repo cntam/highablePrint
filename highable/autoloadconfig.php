3bdd2369cae94fe02b4ee6b21d4f6f91c69e8789
cntam <34434420+cntam@users.noreply.github.com>
2019-01-23 11:05:19 +0800

    20190123 mac

--- highable/autoloadconfig.php
+++ highable/autoloadconfig.php
@@ -3,10 +3,13 @@ session_start();
 error_reporting(0);
 if('localhost' ==  $_SERVER['SERVER_NAME']){
     $online = false;  //判断是否在线
+    $mac = true;
 }elseif ('www.a.cn' ==  $_SERVER['SERVER_NAME']){
     $online = false;  //判断是否在线
+    $mac = false;
 }elseif ('127.0.0.1' ==  $_SERVER['SERVER_NAME']){
     $online = false;  //判断是否在线
+    $mac = false;
 }else{
     $online = true;  //判断是否在线
 }
@@ -15,8 +18,12 @@ if($online){
     require_once '/home/pan/vendor/autoload.php';
 }else{
     //require_once '/Applications/XAMPP/xamppfiles/htdocs/composer/vendor/autoload.php';  //mac mini
-   // require_once '/Users/hongfeitam/vendor/autoload.php';  //macbookPro
-    require '../../../vendor/autoload.php'; //window10
+    if($mac){
+         require_once '/Users/hongfeitam/vendor/autoload.php';  //macbookPro
+    }else{
+        require '../../../vendor/autoload.php'; //window10
+    }
+
 }
 //require '/home/pan/vendor/autoload.php';
 
